247a3d71c1e6dfe8f438565f9b1a5cf6687a0004
Manav-Mistry <msmistry07@gmail.com>
2022-09-17 16:11:35 +0530

    with controller structure

--- Routes/bootcamp.js
+++ Routes/bootcamp.js
@@ -18,20 +18,4 @@ router.route("/:id")
     .put(updateBootcamp)
     .delete(deleteBootcamp);
 
-// router.get("/", (req, res) => {
-//     res.status(200).json({ success: true, msg: "Show all bootcamps" })
-// })
-// router.get("/:id", (req, res) => {
-//     res.status(200).json({ success: true, msg: `Show bootcamps ${req.params.id}` })
-
-// })
-// router.post("/", (req, res) => {
-//     res.status(200).json({ success: true, msg: "Create Bootcamp" })
-
-// })
-// router.put("/:id", (req, res) => {
-//     res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` })
-
-// })
-
 module.exports = router;
