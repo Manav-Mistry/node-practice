2ab11d7fc1368d9319ee0a6edf243033d0c0ecf4
Manav-Mistry <msmistry07@gmail.com>
2022-09-23 17:26:30 +0530

    first mongoose connection

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcamp = require("./Routes/bootcamp")
-
+const connectDB = require("./config/db")
 dotenv.config({ path: "./config/config.env" })
+
+connectDB();
+
 
 const app = express();
 
 app.use("/api/v1/bootcamps", bootcamp);
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log("Server is Running"))
+
+const server = app.listen(PORT, console.log("Server is Running"))
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // close the server
+    server.close(() => process.exit(1));
+})
 
